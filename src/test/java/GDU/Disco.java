9e5523be3c3fc944253272bc20351e1edddceedf
DISCO\grey <grey@disco.com.uy>
2021-07-26 18:34:04 -0300

    Cambios 3

--- src/test/java/GDU/Disco.java
+++ src/test/java/GDU/Disco.java
@@ -1,49 +1,53 @@
-package Disco;
+package GDU;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.*;
 
-public class Login {
+public class Disco {
 
     WebDriver driver;
 
-    //Método que devuelve un objeto de tipo WebDriver para e instanciar el ChromeDriver y setear las property donde está el driver y acceder a la Web deDisco//
-    private WebDriver InicializarWebDisco(String url){
+    @BeforeTest
+    public void setup(){
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-        WebDriver driver= new ChromeDriver();
-        driver.get(url);
-        return driver;
-    }
-
-    //Método que permite seleccionar la Sucursal//
-    @BeforeClass
-    public void Autenticacion() throws InterruptedException {
-        driver= InicializarWebDisco("https://www.disco.com.uy/");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @BeforeClass
+    public void inicializarUrlDisco(){
+        driver.get("https://www.disco.com.uy/");
+    }
+    //Método que permite seleccionar la Sucursal//
+    @BeforeMethod
+    public void AutenticacionDisco()  {
         WebElement Sucursal = driver.findElement(By.id("selec-suc-popup"));
         Select SucursalSeleccionada = new Select(Sucursal);
-        sleep(2000);
         SucursalSeleccionada.selectByValue("5");
-        sleep(2000);
         driver.findElement(By.id("btnConfirmaSucursal")).click();
         driver.findElement(By.id("btnMiPerfil")).click();
         driver.findElement(By.className("perfil-desktop")).click();
+        driver.navigate().to("https://www.disco.com.uy/login");
         //driver.findElement(By.id("MiPerfil")).click();
-        driver.findElement(By.xpath("//*[@href='/_secure/account']")).click();
-        sleep(10000);
+        //driver.findElement(By.xpath("//*[@href='/_secure/account']")).click();
+        //driver.findElement(By.className("perfil-desktop")).click();
         driver.findElement(By.id("loginWithUserAndPasswordBtn")).click();
         driver.findElement(By.id("inputEmail")).sendKeys("grey@disco.com.uy");
         driver.findElement(By.id("inputPassword")).sendKeys("Internet0.");
         driver.findElement(By.id("classicLoginBtn")).click();
-        sleep(2000);
+        //sleep(2000);
     }
     @Test
-    public void Comprar() {
-        driver.findElement(By.xpath("//*[@class='fas fa-chevron-down']")).click();
+    public void ComprarDisco() {
+       // driver.findElement(By.xpath("//*[@class='fas fa-chevron-down']")).click();
     }
 }
